e38f365bbda4d9383fe69aa5f4c5169dd7be1bdb
Veeral Patel <vpatel98@gmail.com>
2015-10-17 22:21:42 -0700

    wrote WeatherData code

---
+++ WeatherData.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+function WeatherData(lat, lon) {
+	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
+	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body)) {
+		if (!error && response.statusCode == 200) {
+			var json = JSON.parse(body)
+	}
+
+	var weatherSummary = json.currently.summary;
+	var icon = currently.icon
+	
+}
