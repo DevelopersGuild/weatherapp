e55d715cf162c60a167bc4593ad674fcb70841ef
Veeral Patel <vpatel98@gmail.com>
2015-10-17 23:17:33 -0700

    added object to store hourly temperature data

--- WeatherData.js
+++ WeatherData.js
@@ -4,10 +4,17 @@ function WeatherData(lat, lon) {
 	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
 	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body)) {
 		if (!error && response.statusCode == 200) {
-			var json = JSON.parse(body)
+			var jsonBody = JSON.parse(body)
 	}
 
-	var weatherSummary = json.currently.summary
-	var icon = json.currently.icon
-	
+	var weatherSummary = jsonBody.currently.summary
+	var icon = jsonBody.currently.icon
+
+	Object hourlyTempData;	//store hourly temperature data
+	for (var key in jsonBody.hourly.data) {
+		if (jsonBody.hasOwnProperty(key) && key == "temperature") {		//filters out prototypes
+			// adds temperature key-value pairs from jsonBody to hourlyData
+			hourlyTempData[key] = jsonBody.hourly.data[key]			
+		}
+	}
 }
