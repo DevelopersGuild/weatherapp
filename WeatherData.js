baab59cac8acc03e71ac8a311bc25ddd23fd5f96
Paul Badalian <bad.alien7@gmail.com>
2015-10-18 18:46:06 -0700

    Changed weather and app files

--- WeatherData.js
+++ WeatherData.js
@@ -2,12 +2,20 @@ var request = require('request');
 
 function WeatherData(lat, lon) {
 	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
+	var weather = ""
 	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body)) {
 		if (!error && response.statusCode == 200) {
-			var json = JSON.parse(body)
+			weather = JSON.parse(body)
+		}
+		else{
+			console.log("Error reading data")
+		}
 	}
 
-	var weatherSummary = json.currently.summary
-	var icon = json.currently.icon
+	
 	
 }
+
+/*
+examples: var data = new WeatherData(37, 100)
+		  var currently = data.weather.currently
