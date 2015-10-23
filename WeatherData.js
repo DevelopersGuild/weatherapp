91ac20963c63208d2094f9260342cb39ea640ce0
Veeral Patel <vpatel98@gmail.com>
2015-10-23 15:08:42 -0700

    updated weather data

--- WeatherData.js
+++ WeatherData.js
@@ -1,30 +1,48 @@
 var request = require('request');
 
-function WeatherData(lat, lon) {
+function getWeatherJSON(lat, lon) {
 	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
 	var weather = ""
-	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body)) {
+	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body) {
 		if (!error && response.statusCode == 200) {
-<<<<<<< HEAD
-			weather = JSON.parse(body)
+			var jsonBody = JSON.parse(body)
 		}
 		else{
 			console.log("Error reading data")
 		}
-	}
 
-	
-	
+		return jsonBody
+		res.send(jsonBody);
+
+	})
+	return 'fuck'
+}
+
+console.log("hi")
+var newJSONObj = getWeatherJSON(20,20)
+console.log(newJSONObj)
+
+function getCurrentTime(lat, lon) {
+	jsonBody = getWeatherJSON(lat, lon)
+	var timeStamp = jsonBody.currently.time
+	var normalTime = new Date(timestamp*1000)
+	return normalTime.dateString
+}
+
+
+function getCurrentSummary(lat, lon) {
+	jsonBody = getWeatherJSON(lat, lon)
+	return jsonBody.currently.summary
+}
+
+function getCurrentIcon(lat, lon) {
+	jsonBody = getWeatherJSON(lat, lon)
+	return jsonBody.currently.icon
 }
 
 /*
-examples: var data = new WeatherData(37, 100)
-		  var currently = data.weather.currently
-=======
-			var jsonBody = JSON.parse(body)
-	}
+var data = new WeatherData(37, 100)
+var weatherSummary = jsonBody.currently.summary
+var icon = jsonBody.currently.icon
 
-	var weatherSummary = jsonBody.currently.summary
-	var icon = jsonBody.currently.icon
-}
->>>>>>> 5cc125f545f4f300391e18c51cb584cbcc2b6235
+*/
