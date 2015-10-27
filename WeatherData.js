d5392fc49a3552038e3390b853a9df0ee3e0f831
Veeral Patel <vpatel98@gmail.com>
2015-10-26 18:33:26 -0700

    minor changes

--- WeatherData.js
+++ WeatherData.js
@@ -1,26 +1,6 @@
 var request = require('request');
 
-function getWeatherJSON(lat, lon) {
-	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
-	var weather = ""
-	request("https://api.forecast.io/forecast/"+apiKey+"/"+lat+","+lon, function(error,response,body) {
-		if (!error && response.statusCode == 200) {
-			var jsonBody = JSON.parse(body)
-		}
-		else{
-			console.log("Error reading data")
-		}
 
-		return jsonBody
-		res.send(jsonBody);
-
-	})
-	return 'fuck'
-}
-
-console.log("hi")
-var newJSONObj = getWeatherJSON(20,20)
-console.log(newJSONObj)
 
 function getCurrentTime(lat, lon) {
 	jsonBody = getWeatherJSON(lat, lon)
--- router/index.js
+++ router/index.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var weather;
 
 /* GET home page. */
 router.get('/weather/:lat/:long', function(req, res, next) {
   	var apiKey = "442f6fad9a9871839e00c77d18cc2a51";	//my API key
-	var weather = ""
+	weather = ""
 	request("https://api.forecast.io/forecast/"+apiKey+"/"+req.params.lat+","+req.params.long, function(error,response,body){
 		if (!error && response.statusCode == 200) {
 			weather = JSON.parse(body)
-			res.send(weather);
+			//res.send(weather);
 		}
 		else{
 			res.send("Error reading data");
 		}
 	})
 });
+
+router.get('/weather/impdata/:lat/:long', function(req, res, next) {
+	res.send("Current Summary:" + getCurrentSummary() + "\n" + "Current Icon: " + getCurrentIcon() + "\n" + "Current Time:" + getCurrentTime);
+});
+
+function getCurrentTime() {
+	var timeStamp = weather.currently.time
+	var normalTime = new Date(timestamp*1000)
+	return normalTime.dateString
+}
+
+
+function getCurrentSummary() {
+	return weather.currently.summary
+}
+
+function getCurrentIcon() {
+	return weather.currently.icon
+}
+
 
 module.exports = router;
